9c89a10292ed132b2a10bb2e9cd4302ba348f8f0
glejeune <gregoire.lejeune@free.fr>
2015-02-23 20:59:19 +0100

    Add generator for generator

---
+++ plugins/paris_generator_generator.erl
@@ -0,0 +1,28 @@
+-module(paris_generator_plugin).
+
+-export([generate/3]).
+
+generate(Config, Options, Args) ->
+  case (elists:include(Options, help) orelse Args =:= []) of
+    true -> help();
+    false -> g(Config, Args)
+  end.
+
+help() ->
+  paris_log:print("Usage:"),
+  paris_log:print("~s generate generator <name> [options]", [paris:get_script_name()]),
+  paris_log:print("~nOptions:~n"),
+  paris_log:print("     --help           : Display this help").
+
+g(Config, [Name|_]) ->
+  paris_log:debug("* Generate generator ~s...", [Name]),
+  PluginsPath = paris_config:paris_plugins_dir(Config),
+  GeneratorModuleFile = filename:join([PluginsPath, "paris_generator_" ++ eutils:to_string(Name) ++ ".erl"]),
+  case paris_generator_plugin_dtl:render([{name, eutils:to_string(Name)}]) of
+    {ok, Data} ->
+      paris_log:info("* Generate ~s", [GeneratorModuleFile]),
+      file:write_file(GeneratorModuleFile, Data);
+    _ ->
+      paris_log:stop("! Can't create ~s", [GeneratorModuleFile])
+  end.
+
